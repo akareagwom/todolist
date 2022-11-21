278759ce6daa3eca0c754543e43573a8443f373e
akareagwom <103905242+akareagwom@users.noreply.github.com>
2022-11-21 07:05:17 +0100

    first dark mode

--- public/script.js
+++ public/script.js
@@ -2,6 +2,16 @@ var input = document.getElementById("field");
 var check = document.getElementById("enter");
 var ul = document.querySelector("ul");
 var clear = document.getElementById("clear");
+var toggle = document.getElementById("toggleDark");
+var body = document.querySelector('body');
+
+
+toggle.addEventListener('click', function(){
+    document.getElementById('mainImage').src = "images/bg-desktop-dark.jpg";
+    document.getElementById("toggleDark").src = "images/icon-sun.svg"
+    // this.classList.toggle('url(./images/icon-sun.svg)')
+    
+})
 
 check.addEventListener("click",function(){
     
